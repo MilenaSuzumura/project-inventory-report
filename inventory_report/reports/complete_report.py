62da5df6e82b58739e66476c58b274b856b9fea1
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-04 20:32:49 -0300

    requisito 3 talvez feito

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(self):
+        topInfo = SimpleReport.generate(self)
+        arrayNome = []
+        estoque = ''
+
+        for product in self:
+            arrayNome.append(product['nome_da_empresa'])
+
+        nomeEmpresa = Counter(arrayNome).most_common()
+        for empresa in nomeEmpresa:
+            estoque += f"- {empresa[0]}: {empresa[1]}\n"
+
+        return (
+            f"{topInfo}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{estoque}"
+        )
