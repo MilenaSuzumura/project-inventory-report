e6bf2477531e3989d5061bd84fb10ff714096cd0
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-04 19:41:36 -0300

    requisito 2 talvez feito

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,28 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(self):
+        timeNow = datetime.now().strftime("%Y-%m-%d")
+        fabricacaoMaisAntiga = timeNow
+        arrayValidade = []
+        arrayNome = []
+
+        for product in self:
+            if fabricacaoMaisAntiga > product['data_de_fabricacao']:
+                fabricacaoMaisAntiga = product['data_de_fabricacao']
+
+            if timeNow < product['data_de_validade']:
+                arrayValidade.append(product['data_de_validade'])
+            arrayNome.append(product['nome_da_empresa'])
+
+        """ info: https://docs.python.org/3/library/collections.html """
+        nomeEmpresa = Counter(arrayNome).most_common(1)
+
+        return (
+            f"Data de fabricação mais antiga: {fabricacaoMaisAntiga}\n"
+            f"Data de validade mais próxima: {min(arrayValidade)}\n"
+            f"Empresa com mais produtos: {nomeEmpresa[0][0]}"
+        )
